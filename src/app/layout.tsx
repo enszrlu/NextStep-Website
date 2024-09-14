bedfc6afa0523e696fb656f1f8305c5ff85922a1
enszrlu <65814876+enszrlu@users.noreply.github.com>
2024-09-14 21:01:07 +0100

    improve metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,50 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://nextstepjs.vercel.app/'),
   title: 'NextStep',
   description: 'Lightweight onboarding library for Next.js',
+  openGraph: {
+    images: [
+      {
+        url: '/ogThumbnail.png',
+        width: 1200,
+        height: 630,
+        alt: 'NextStep - Lightweight onboarding library for Next.js',
+      },
+    ],
+    type: 'website',
+    siteName: 'NextStep',
+    title: 'NextStep',
+    description: 'Lightweight onboarding library for Next.js',
+    url: 'https://nextstepjs.vercel.app/',
+    locale: 'en_US',
+  },
+  twitter: {
+    images: [
+      {
+        url: '/xThumbnail.png',
+        width: 1200,
+        height: 600,
+        alt: 'NextStep - Lightweight onboarding library for Next.js',
+      },
+    ],
+    site: '@AlexZDevs',
+    creator: '@AlexZDevs',
+    title: 'NextStep',
+    description: 'Lightweight onboarding library for Next.js',
+    card: 'summary_large_image',
+  },
+  appleWebApp: {
+    title: 'NextStep',
+    startupImage: '/ogThumbnail.png',
+  },
+  applicationName: 'NextStep',
+  icons: {
+    icon: '/icon.png',
+    shortcut: '/icon.png',
+    apple: '/icon.png',
+  },
 };
 
 export default function RootLayout({
