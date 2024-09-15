c4ec3e35923b460d0477ab1fb68a082a03c4de9a
enszrlu <65814876+enszrlu@users.noreply.github.com>
2024-09-15 08:35:25 +0100

    Fix package name in tailwindconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-		'./node_modules/nextstep/dist/**/*.{js,ts,jsx,tsx}' // NextStep
+		'./node_modules/nextstepjs/dist/**/*.{js,ts,jsx,tsx}' // NextStep
   ],
   theme: {
   	extend: {
