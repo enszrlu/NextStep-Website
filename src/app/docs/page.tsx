30771fe02eebb3cdc57b6375e4987faa86700923
enszrlu <65814876+enszrlu@users.noreply.github.com>
2024-09-22 21:00:36 +0100

    remove radix portal

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,10 +1,5 @@
 import DocsPage from '@/components/DocsPage';
-import { Suspense } from 'react';
 
 export default function Docs() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <DocsPage />
-    </Suspense>
-  );
+  return <DocsPage />;
 }
